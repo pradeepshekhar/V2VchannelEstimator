78f479da9534806184c6695ef126bfc7f5f67af8
pradeepshekhar <pradeepshekhar@users.noreply.github.com>
2017-03-27 15:00:22 -0700

    Added comments for better understanding

--- Channel_estimation.m
+++ Channel_estimation.m
@@ -1,3 +1,6 @@
+%using CVX tool box to estimate the channel impulse response from channel's input and output data
+% Since it is a sparse channel, using Compressed Sensing
+
 K = 50; 
 M = 180;
 Nr = 100;
@@ -8,8 +11,9 @@ cvx_begin
     variable x(N)
     minimize( norm( x, 1) )
     subject to
-        norm((y_n - S*x),2) <= 0.005;%abs(y_n - S*x) <= 0.005;
+        norm((y_n - S*x),2) <= 0.005; 
 cvx_end
-%x_mod = abs(x) ;
-%figure(1);
-%bar(x_mod);
+
+x_mod = abs(x) ;
+figure(1);
+bar(x_mod);
